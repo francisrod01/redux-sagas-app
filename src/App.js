b0390b5e550912b370acb32f02d0f7309155930b
Francis Rodrigues <francis.rod01@outlook.com>
2018-08-25 16:25:08 -0300

    Added Router tag on App component

--- src/App.js
+++ src/App.js
@@ -16,18 +16,22 @@ class App extends Component {
   render() {
     return (
       <Provider store={reduxStore}>
-        <div className="App">
+        <Router>
 
-          <Route path='/login' component={Login} />
+          <div className="App">
 
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-        </div>
+            <Route path='/login' component={Login} />
+
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to React</h1>
+            </header>
+            <p className="App-intro">
+              To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+          </div>
+
+        </Router>
       </Provider>
     );
   }
